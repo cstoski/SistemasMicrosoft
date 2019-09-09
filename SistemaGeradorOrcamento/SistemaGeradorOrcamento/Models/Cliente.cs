4353c7381948300e7caad8c6e291cb55ffc9cd4e
Cristiano Stoski <cristianostoski@hotmail.com>
2019-09-09 12:02:20 -0300

    Inclusão de Classe projeto

---
+++ SistemaGeradorOrcamento/SistemaGeradorOrcamento/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGeradorOrcamento.Models
+{
+    //teste
+    /// <summary>
+    /// 
+    /// </summary>
+    [Table("Clientes")]
+    class Cliente
+    {
+        [Key] //Define chave primária 
+        public int clienteId { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public String nomeCliente { get; set; }
+        public String contato { get; set; }
+        public String telefone { get; set; }
+
+        //Comentários
+       
+    }
+}
--- SistemaGeradorOrcamento/SistemaGeradorOrcamento/Models/Projeto.cs
+++ SistemaGeradorOrcamento/SistemaGeradorOrcamento/Models/Projeto.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SistemaGeradorOrcamento.Models
 {
+    [Table("Projetos")]
     class Projeto
     {
-     
-        public static void Commit()
+        //Construtor da Classe
+     public Projeto()
         {
-            //CLASSE APENAS PARA TESTE DE COMMIT
-            //Teste OK
+            CriadoEm = DateTime.Now;
         }
+        [Key] //Define chave primária 
+        public int projetoId { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        public String numeroProjeto { get; set; }
+        public String nomeProjeto { get; set; }
+        public String cliente { get; set; }
+        public enum status
+        {
+            
+        }
+        //public List<Orcamento> listaOrcamento { get; set; }
 
     }
 }
