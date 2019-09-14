0fbe2a1c2152880c5e0bbdae54412293fe71ad6b
Cristiano Stoski <cristianostoski@hotmail.com>
2019-09-14 14:00:10 -0300

    Criado Projeto Dao

---
+++ SistemaGeradorOrcamento/SistemaGeradorOrcamento/DAL/ProjetoDao.cs
@@ -0,0 +1,33 @@
+﻿using SistemaGeradorOrcamento.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGeradorOrcamento.DAL
+{
+    class ProjetoDao
+    {
+        private static Context ctx = new Context();
+
+        public static bool CadastrarProjeto(Projeto p)
+        {
+            if (BuscarProjetoPorNome(p) == null)
+            {
+                ctx.Projetos.Add(p);
+                ctx.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+       
+        public static Projeto BuscarProjetoPorNome(Projeto p)
+        {
+            return ctx.Projetos.FirstOrDefault
+                (x => x.NomeProjeto.Equals(p.NomeProjeto));
+        }
+
+    }
+}
