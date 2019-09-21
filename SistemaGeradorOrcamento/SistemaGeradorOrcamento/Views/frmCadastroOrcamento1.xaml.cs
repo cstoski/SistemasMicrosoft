f4c4fe7c118e8ffbe271400e27a67137ba399f51
Cristiano Stoski <cristianostoski@hotmail.com>
2019-09-21 12:08:46 -0300

    Frame temporário para teste de Orçamento com inserção de Serviço

---
+++ SistemaGeradorOrcamento/SistemaGeradorOrcamento/Views/frmCadastroOrcamento1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace SistemaGeradorOrcamento.Views
+{
+    /// <summary>
+    /// Interaction logic for frmCadastroOrcamento1.xaml
+    /// </summary>
+    public partial class frmCadastroOrcamento1 : Window
+    {
+        public frmCadastroOrcamento1()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
